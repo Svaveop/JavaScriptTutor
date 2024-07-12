b1e16f0c11ec2df0c85cad3a3f0bd1e205665c4e
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-12 23:32:53 +0400

    Create Variables.js

---
+++ Variables.js
@@ -0,0 +1,39 @@
+let animals = ["horse","lion","eagle","tiger"]
+console.log(animals)
+
+ animals[4] = "cat"
+ console.log(animals)
+
+ animals[0] = "unicorn"
+ console.log(animals)
+
+animals[5] = "dog"
+console.log(animals)
+
+animals[6] = "mouse"
+console.log(animals)
+
+animals[7] = "zebra"
+console.log(animals)
+
+let week = ["ekushabti"]
+console.log(week)
+
+week[1] = "ereqshabti"
+console.log(week)
+
+week[2] = "hingsabti"
+console.log(week)
+
+week[3] = "urbat"
+console.log(week)
+
+week[4] = "shabat"
+console.log(week)
+
+week[5] = "kiraki"
+console.log(week)
+
+var n1 = "Vahe"
+var n2 = "Martirosyan"
+var n3 = n1 + " " +n2
