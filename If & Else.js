80b875b338a5c1fc5176700a61b046caec9e6b57
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-12 23:54:29 +0400

    Create If & Else.js

---
+++ If & Else.js
@@ -0,0 +1,43 @@
+if(age<18)
+{
+    console.log("Welcome !")
+}
+else
+{
+    console.log("You're too old !")
+}
+
+console.log("You're " + age + " old")
+
+
+
+var word = prompt("Enter animal's/plant's name")
+
+if(word === "cat")
+{
+    console.log(word + " is a mammal")
+}
+else if(word === "snake")
+{
+    console.log(word + " is a reptile")
+}
+else if(word === "Eagle")
+{
+    console.log(word + " is a bird")
+}
+else if(word === "Horse")
+{
+    console.log(word + " is a mamal")
+}
+else if(word === "Crab")
+{
+    console.log(word + "is an amphibian")
+}
+else if(word === "Brier")
+{
+    console.log(word + "is a Shrub")
+}
+else
+{
+    console.log(word + "No info about that word")
+}
