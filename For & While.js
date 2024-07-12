612931f42445606224c4ac8fc8286aa43d349e7e
Vahe <168283825+Svaveop@users.noreply.github.com>
2024-07-12 23:42:22 +0400

    Create For & While.js

---
+++ For & While.js
@@ -0,0 +1,26 @@
+//for
+for(var i = 1; i <= 20; i++)
+{
+    console.log("hajord tiv@")
+    console.log(i)
+}
+for(var v = 5; v >= 60;v--)
+{
+    console.log(v)
+}
+
+//while
+var i = 1
+
+while(i<=20){
+        console.log(i)
+        i++
+}
+
+
+var i = 5
+
+while(i<=60){
+        console.log(i)
+        i++
+}
